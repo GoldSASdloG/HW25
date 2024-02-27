ca8c1adbc97d04503154f1119c04515e771a81e4
GoldSASdloG <Procatulus@gmail.com>
2024-02-27 16:00:59 +0300

    home11 - okay - lambda

--- HW25/src/homeWork25/MainHome.java
+++ HW25/src/homeWork25/MainHome.java
@@ -1,6 +1,7 @@
 package homeWork25;
 
 import java.util.Scanner;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class MainHome {
@@ -18,10 +19,16 @@ public class MainHome {
             return human;
         };
         Human human1 = humanVasy.get();
-//        Human human2 = humanVasy.get();
-//        Human human3 = humanVasy.get();
+        Human human2 = humanVasy.get();
+        Human human3 = humanVasy.get();
 
         System.out.println(human1);
+
+        Predicate<Integer> isPasportVozrast = (age) -> age >= 18;
+
+        System.out.println(human1.getName() + " - " + (isPasportVozrast.test(human1.getAge()) ? "молодой" : "старый"));
+        System.out.println(human2.getName() + " - " + (isPasportVozrast.test(human2.getAge()) ? "молодой" : "старый"));
+        System.out.println(human3.getName() + " - " + (isPasportVozrast.test(human3.getAge()) ? "молодой" : "старый"));
 
     }
 }
